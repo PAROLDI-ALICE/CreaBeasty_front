ebbe2bd864cea9c37836b9c2f41522817eb6eae5
JeremieG06 <jeremie.garod@epitech.eu>
2023-06-02 11:55:53 +0200

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import '../Styles.css'
+import '../Styles.css';
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,37 +10,32 @@ function Navbar() {
   };
 
   return (
-    <nav className="navbar">
+    <nav className="navbar navbar-light amber lighten-4 mb-4">
       <div className="logo">
-        <h1>Jeremie Garod</h1>
+        <h1>Navbar</h1>
       </div>
-
-      <div
-        className={`menu-toggle ${isMenuOpen ? "active" : ""}`}
-        onClick={toggleMenu}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
+      <button className={`navbar-toggler first-button ${isMenuOpen ? "active" : ""}`} type="button" onClick={toggleMenu}>
+        <div className="animated-icon1"><span></span><span></span><span></span></div>
+      </button>
+      <div className={`collapse navbar-collapse ${isMenuOpen ? "open" : ""}`} id="navbarSupportedContent20">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link to="/" className="nav-link" onClick={toggleMenu}>
+              Home <span className="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/features" className="nav-link" onClick={toggleMenu}>
+              Features
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/pricing" className="nav-link" onClick={toggleMenu}>
+              Pricing
+            </Link>
+          </li>
+        </ul>
       </div>
-
-      <ul className={`nav-links ${isMenuOpen ? "open" : ""}`}>
-        <li>
-          <Link to="/" className="nav-link" onClick={toggleMenu}>
-            Accueil
-          </Link>
-        </li>
-        <li>
-          <Link to="/projets" className="nav-link" onClick={toggleMenu}>
-            Projets
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className="nav-link" onClick={toggleMenu}>
-            Contact
-          </Link>
-        </li>
-      </ul>
     </nav>
   );
 }
