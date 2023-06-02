d4adef39069b71569ff39de43d3cda6106591511
PAROLDI-ALICE <aliceparoldi@gmail.com>
2023-06-02 11:37:46 +0200

    Update imports

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
+import React from "react";
 
-
-
-
+function Home() {
+  return <div></div>;
+}
+export default Home;
---
+++ src/components/Products.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Products() {
+  return <div></div>;
+}
+export default Products;
