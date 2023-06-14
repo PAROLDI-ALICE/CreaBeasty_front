68cad8e5584c2352b1b5b50bfe0d89b2a9be07e0
JeremieG06 <jeremie.garod@epitech.eu>
2023-06-14 11:59:51 +0200

    updtaes connexion.js

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import "../Styles.css";
+import moon from "../assets/Images/moon.jpeg";
 
 function Connexion() {
   const [activeTab, setActiveTab] = useState("contact");
@@ -106,34 +107,8 @@ function Connexion() {
             role="tabpanel"
             aria-labelledby="contact-tab"
           >
-            <div className="card-body card-content">
-              <h4>CONTACT</h4>
-              <p>Ceci est le contenu de l'onglet Contact.</p>
-            </div>
-          </div>
-          <div
-            className={`tab-pane fade ${
-              activeTab === "connexion" ? "show active" : ""
-            }`}
-            id="connexion"
-            role="tabpanel"
-            aria-labelledby="connexion-tab"
-          >
-            <div className="card-body card-content">
-              <h4>CONNEXION</h4>
-              <p>Ceci est le contenu de l'onglet Connexion.</p>
-            </div>
-          </div>
-          <div
-            className={`tab-pane fade ${
-              activeTab === "inscription" ? "show active" : ""
-            }`}
-            id="inscription"
-            role="tabpanel"
-            aria-labelledby="inscription-tab"
-          >
             <div className="card-body card-content form-container">
-              <h4>INSCRIPTION</h4>
+              <h4>CONTACT</h4>
               <div className="Rappel">
                 <span>Rappel Entreprise</span>
               </div>
@@ -141,9 +116,8 @@ function Connexion() {
                 Lorem Ipsum is simply dummy text of the printing.{" "}
               </p>
               <br />
-              <div></div>
               <div className="Rappel">
-                <span>Remerciement</span>
+                <span>Coordonnées</span>
               </div>
               <p className="Rappel1">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -175,37 +149,25 @@ function Connexion() {
                     onChange={handleChange}
                   />
                 </div>
+
                 <div>
-                  <label htmlFor="username">Pseudo :</label>
+                  <label htmlFor="password">Sujet</label>
                   <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    value={formData.username}
+                    type="sujet"
+                    id="sujet"
+                    name="sujet"
+                    value={formData.sujet}
                     onChange={handleChange}
                   />
                 </div>
                 <div>
-                  <label htmlFor="password">Mot de passe :</label>
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={formData.password}
+                  <label for="message">Message</label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    value={formData.message}
                     onChange={handleChange}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="confirmPassword">
-                    Confirmer le mot de passe :
-                  </label>
-                  <input
-                    type="password"
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    value={formData.confirmPassword}
-                    onChange={handleChange}
-                  />
+                  ></textarea>
                 </div>
                 <div>
                   <label htmlFor="captcha">Captcha :</label>
@@ -231,6 +193,167 @@ function Connexion() {
                 </button>
               </form>
             </div>
+          </div>
+          <div
+            className={`tab-pane fade ${
+              activeTab === "connexion" ? "show active" : ""
+            }`}
+            id="connexion"
+            role="tabpanel"
+            aria-labelledby="connexion-tab"
+          >
+            <div className="card-body card-content form-container">
+              <h4>CONNEXION</h4>
+
+              <p className="Rappel1"></p>
+              <br />
+              <div className="Rappel">
+                <img src={moon} alt="lune" />
+              </div>
+
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="email">Email :</label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="password">Mot de passe :</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                  />
+                </div>
+                <br/>
+
+                <div>
+                  <button className="button_form" type="submit">
+                    Mot de passe oublié
+                  </button>
+                </div>
+
+                <br />
+                <button className="button_form" type="submit">
+                  Envoyer
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div
+          className={`tab-pane fade ${
+            activeTab === "inscription" ? "show active" : ""
+          }`}
+          id="inscription"
+          role="tabpanel"
+          aria-labelledby="inscription-tab"
+        >
+          <div className="card-body card-content form-container">
+            <h4>INSCRIPTION</h4>
+            <div className="Rappel">
+              <span>Rappel Entreprise</span>
+            </div>
+            <p className="Rappel1">
+              Lorem Ipsum is simply dummy text of the printing.{" "}
+            </p>
+            <br />
+            <div className="Rappel">
+              <span>Remerciement</span>
+            </div>
+            <p className="Rappel1">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </p>
+
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="name">Nom :</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email :</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="username">Pseudo :</label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="password">Mot de passe :</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="confirmPassword">
+                  Confirmer le mot de passe :
+                </label>
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="captcha">Captcha :</label>
+                <ReCAPTCHA
+                  sitekey="VOTRE_CLÉ_DE_SITE_RECAPTCHA"
+                  onChange={handleChangeCaptcha}
+                />
+              </div>
+              <div>
+                <label>
+                  <input
+                    className="checkbox"
+                    type="checkbox"
+                    name="gdpr"
+                    checked={formData.gdpr}
+                    onChange={handleChange}
+                  />
+                  Accepter les conditions RGPD
+                </label>
+              </div>
+              <button className="button_form" type="submit">
+                Envoyer
+              </button>
+            </form>
           </div>
         </div>
       </div>
