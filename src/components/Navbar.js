f855aebc85bed8d47ec93d36b05f349ad1dbe7f4
PAROLDI-ALICE <aliceparoldi@gmail.com>
2023-06-15 16:46:51 +0200

    Update Footer & style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { FaHome, FaUser, FaShoppingCart, FaEnvelope, FaPowerOff, FaPlay } from "react-icons/fa";
+import {
+  FaHome,
+  FaUser,
+  FaShoppingCart,
+  FaEnvelope,
+  FaPowerOff,
+  FaPlay,
+} from "react-icons/fa";
 import "../Styles.css";
 
 function Navbar() {
@@ -15,46 +22,74 @@ function Navbar() {
       <div className="logo">
         <h1>CreaBeasty</h1>
       </div>
-      <button className={`navbar-toggler first-button ${isMenuOpen ? "active" : ""}`} type="button" onClick={toggleMenu}>
-        <div className="animated-icon1">
-          <span></span>
-          <span></span>
-          <span></span>
+      <div className="navContent-right">
+        <div className="iconNavContent">
+          <Link to="/connexion" className="nav-link1">
+            <FaUser className="icon-navbar" />
+          </Link>
         </div>
-      </button>
-      <div className={`navbar-collapse ${isMenuOpen ? "open" : ""}`}>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink to="/" className="nav-link1" onClick={toggleMenu}>
-              Accueil<span className="sr-only"></span> <FaHome className="icon-right" />
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <Link to="/boutique" className="nav-link1" onClick={toggleMenu}>
-              Boutique <span className="sr-only"></span> <FaPlay className="icon-right" />
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/panier" className="nav-link1" onClick={toggleMenu}>
-              Panier <span className="sr-only"></span> <FaShoppingCart className="icon-right" />
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact" className="nav-link1" onClick={toggleMenu}>
-              Contact <span className="sr-only"></span> <FaEnvelope className="icon-right" />
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/connexion" className="nav-link1" onClick={toggleMenu}>
-              Connexion / Inscription <span className="sr-only"></span> <FaUser className="icon-right" />
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/deconnexion" className="nav-link1" onClick={toggleMenu}>
-              Déconnexion <span className="sr-only"></span> <FaPowerOff className="icon-right" />
-            </Link>
-          </li>
-        </ul>
+        <div className="iconNavContent">
+          <Link to="/panier" className="nav-link1">
+            <FaShoppingCart className="icon-navbar" />
+          </Link>
+        </div>
+        <button
+          className={`navbar-toggler first-button ${
+            isMenuOpen ? "active" : ""
+          }`}
+          type="button"
+          onClick={toggleMenu}
+        >
+          <div className="animated-icon1">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </button>
+        <div className={`navbar-collapse ${isMenuOpen ? "open" : ""}`}>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link1" onClick={toggleMenu}>
+                Accueil<span className="sr-only"></span>{" "}
+                <FaHome className="icon-right" />
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <Link to="/boutique" className="nav-link1" onClick={toggleMenu}>
+                Boutique <span className="sr-only"></span>{" "}
+                <FaPlay className="icon-right" />
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/panier" className="nav-link1" onClick={toggleMenu}>
+                Panier <span className="sr-only"></span>{" "}
+                <FaShoppingCart className="icon-right" />
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link1" onClick={toggleMenu}>
+                Contact <span className="sr-only"></span>{" "}
+                <FaEnvelope className="icon-right" />
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/connexion" className="nav-link1" onClick={toggleMenu}>
+                Connexion / Inscription <span className="sr-only"></span>{" "}
+                <FaUser className="icon-right" />
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/deconnexion"
+                className="nav-link1"
+                onClick={toggleMenu}
+              >
+                Déconnexion <span className="sr-only"></span>{" "}
+                <FaPowerOff className="icon-right" />
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
-import '../Styles.css';
+import { FaShoppingCart, FaEnvelope } from "react-icons/fa";
+import "../Styles.css";
 
 function Footer() {
   return (
     <footer className="footer amber lighten-4">
       <div className="footer-content">
-        
+        <div className="text-footer-icon">
+          <div className="iconFooterContent">
+            <Link to="/contact" className="nav-link1">
+              <FaEnvelope /> Contact
+            </Link>
+          </div>
+          <div className="iconFooterContent">
+            <Link to="/panier" className="nav-link1">
+              <FaShoppingCart /> Commande
+            </Link>
+          </div>
+        </div>
         <div className="footer-social">
           <a href="https://facebook.com" className="footer-social-link">
             <FaFacebook className="footer-social-icon" />
@@ -17,6 +30,14 @@ function Footer() {
           <a href="https://instagram.com" className="footer-social-link">
             <FaInstagram className="footer-social-icon" />
           </a>
+        </div>
+        <div className="text-footer">
+          <div className="footerText">
+            <p>Copyright</p>
+          </div>
+          <div className="footerText">
+            <p>Mentions Légales</p>
+          </div>
         </div>
       </div>
     </footer>
